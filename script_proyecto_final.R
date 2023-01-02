6c998a02e1d98aac3cc5868f3ec01ec95eb1e751
Mayra Mendoza <119996949+may-pixxy@users.noreply.github.com>
2023-01-02 13:26:21 -0600

    Add files via upload

--- script_proyecto_final.R
+++ script_proyecto_final.R
@@ -12,12 +12,59 @@ library (stringr)
 # El primer paso es colocar la base de datos con la que trabajamos aqui. 
 View (recetas)
 
-# El primer paso es delimitar una funcion que nos permita obtener datos del usuario 
+# Delimitar una funcion que nos permita obtener datos del usuario 
+
 rectsf <- function () {
   nombre <- readline (prompt = "Hola, ¿cual es tu nombre?: ")
+  print (paste("Hola", nombre, "a continuacion te hare algunas preguntas sobre los ingredientes con los que deseas cocinar, es importante que coloques la cantidad de ingredientes en numero (ej. 4), coloca el nombre en minusculas, sin acentos o espacios y si tu ingrediente posee una letra 'ñ' puedes sustituirla por 'gn' (ej. champiñones -> champignones). Gracias!"))
   
-  print (paste("Hola", nombre, "a continuacion necesitamos que nos indiques 6 ingredientes que poseeas en tu cocina con los que desees trabajar, si tienes 5 en lugar de 6 ingredientes puedes indicarnoslo poniendo 'si' en el nombre del ingrediente. Es importante que no introduzcas acentos o espacios al final de los nombres. Si alguno de tus ingredientes posee una letra 'ñ' puedes escribirla como gn. Ejemplo = champignones, gracias. "))
+  cts <- readline (prompt = "¿cuantos ingredientes posees? (max = 6): ")
+
+if (cts == "1") {
+  i1 <- readline(prompt = "¿cual es tu ingrediente?: ")
+  lista <- subset(recetas, grepl(i1, ingred))
   
+} else if (cts == "2"){
+  i1 <- readline (prompt = "¿cual es tu primer ingrediente?: ")
+  i2 <- readline (prompt = "¿cual es tu segundo ingrediente?: ")
+  
+  lista1 <- subset(recetas, grepl(i1, ingred))
+  lista <- subset(lista1, grepl(i2, ingred))
+  
+} else if (cts == "3"){
+  i1 <- readline (prompt = "¿cual es tu primer ingrediente?: ")
+  i2 <- readline (prompt = "¿cual es tu segundo ingrediente?: ")
+  i3 <- readline (prompt = "¿cual es tu tercer ingrediente?: ")
+  
+  lista1 <- subset(recetas, grepl(i1, ingred))
+  lista2 <- subset(lista1, grepl(i2, ingred))
+  lista <-  subset(lista2, grepl(i3, ingred))
+  
+} else if (cts == "4") {
+  i1 <- readline (prompt = "¿cual es tu primer ingrediente?: ")
+  i2 <- readline (prompt = "¿cual es tu segundo ingrediente?: ")
+  i3 <- readline (prompt = "¿cual es tu tercer ingrediente?: ")
+  i4 <- readline (prompt = "¿cual es tu cuarto ingrediente?: ")
+  
+  lista1 <- subset(recetas, grepl(i1, ingred))
+  lista2 <- subset(lista1, grepl(i2, ingred))
+  lista3 <-  subset(lista2, grepl(i3, ingred))
+  lista <-  subset(lista3, grepl(i4, ingred))
+  
+} else if (cts == "5"){
+  i1 <- readline (prompt = "¿cual es tu primer ingrediente?: ")
+  i2 <- readline (prompt = "¿cual es tu segundo ingrediente?: ")
+  i3 <- readline (prompt = "¿cual es tu tercer ingrediente?: ")
+  i4 <- readline (prompt = "¿cual es tu cuarto ingrediente?: ")
+  i5 <- readline (prompt = "¿cual es tu quinto ingrediente?: ")
+  
+  lista1 <- subset(recetas, grepl(i1, ingred))
+  lista2 <- subset(lista1, grepl(i2, ingred))
+  lista3 <-  subset(lista2, grepl(i3, ingred))
+  lista4 <-  subset(lista3, grepl(i4, ingred))
+  lista <- subset (lista4, grepl(i5, ingred))
+  
+} else if (cts == "6") {
   i1 <- readline (prompt = "¿cual es tu primer ingrediente?: ")
   i2 <- readline (prompt = "¿cual es tu segundo ingrediente?: ")
   i3 <- readline (prompt = "¿cual es tu tercer ingrediente?: ")
@@ -25,42 +72,30 @@ rectsf <- function () {
   i5 <- readline (prompt = "¿cual es tu quinto ingrediente?: ")
   i6 <- readline (prompt = "¿cual es tu sexto ingrediente?: ")
   
-  lista <- subset(recetas, grepl(i1, ingred))
-  
-  
-  if (i2 == "si") {
-    especif <- readline (prompt = "¿desea hacer una busqueda mas especifica?: ")
-  } else if (i2 != "si") {
-    lista_2 <- subset(lista, grepl(i2, ingred))
-  }
-  
-  if (i3 == "si") {
-    especif <- readline (prompt = "¿desea hacer una busqueda mas especifica?: ")
-  } else if (i3 != "si") {
-    lista_3 <- subset(lista_2, grepl(i3, ingred))
-  } 
-  
-  
-  if (i4 == "si") {
-    especif <- readline (prompt = "¿desea hacer una busqueda mas especifica?: ")
-  } else if (i4 != "si") {
-    lista_4 <- subset(lista_3, grepl(i4, ingred))
-  }
-  
-  
-  if (i5 == "si") {
-    especif <- readline (prompt = "¿desea hacer una busqueda mas especifica?: ")
-  } else if (i5 != "si") {
-    lista_5 <- subset(lista_4, grepl(i5, ingred))
-  } 
-  
-  if (i6 == "si") {
-    especif <- readline (prompt = "¿desea hacer una busqueda mas especifica?: ")
-  } else if (i6 != "si") {
-    lista_6 <- subset(lista_5, grepl(i6, ingred))
-  }
-  
-  View (lista_6) 
+  lista1 <- subset(recetas, grepl(i1, ingred))
+  lista2 <- subset(lista1, grepl(i2, ingred))
+  lista3 <-  subset(lista2, grepl(i3, ingred))
+  lista4 <-  subset(lista3, grepl(i4, ingred))
+  lista5 <- subset (lista4, grepl(i5, ingred))
+  lista <- subset (lista5, grepl(i6, ingred))
 }
+  
+# Hasta aqui terminamos la primera parte del filtraje, primero colocamos una introduccion para que el 
+# usuario entienda las condiciones en que necesitamos que los ingredientes sean escritos para una correcta
+# busqueda 
+  
+# Una vez establecido eso preguntamos la cantidad de ingredientes, debido a la longitud de la base de datos
+# y que las recetas que contiene son de escasos ingredientes, decidimos colocar un numero finito de ingredientes
+# que son 6, esto ya hace muy especifica la busqueda. 
+
+# Ahora, en funcion del digito ingresado por el usuario, R va a preguntarle que ingredientes estara usando 
+# para cocinar, si tiene 3 ingredientes realizara 3 preguntas y asi sucesivamente, el resultado de estas
+# preguntas es la extraccion de una cantidad x de recetas de la base de datos 
+  
+}
+View (lista)
+rectsf()  
+
+
 
 
